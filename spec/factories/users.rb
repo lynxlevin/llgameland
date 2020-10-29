d76e9acf671dec1532eda0951293c08753a78f46
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 11:06:29 +0900

    userモデルの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username              { Faker::Internet.username }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '正常な動作の確認' do
+      it 'usernameとemailとpasswordとpassword_confirmationが存在すれば登録できること' do
+        expect(@user).to be_valid
+      end
+
+      it 'emailがなくても登録できること' do
+        @user.email = nil
+        expect(@user).to be_valid
+      end
+    end
+
+    context '登録できない時の確認' do
+      it 'usernameが空欄では登録ができないこと' do
+        @user.username = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Username can't be blank")
+      end
+
+      it 'usernameが既に使用されているときは登録ができないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user, username: @user.username)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Username has already been taken')
+      end
+
+      it 'passwordが空欄では登録ができないこと' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordとpassword_confirmationが一致しないと登録ができないこと' do
+        @user.password_confirmation = @user.password + "1"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
 end
