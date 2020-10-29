38b2348075ce348b15e724b215fa85e8de912ae0
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 10:02:24 +0900

    password最低文字数の修正漏れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 4..128
   validates :username, presence: true, uniqueness: true
 
   def email_required?
