d422c9899ae7f6511ba63d6f327d83ab5407b063
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 18:03:48 +0900

    不正ユーザーのリダイレクト

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,18 @@
 class GamesController < ApplicationController
+  before_action :redirect_ileligible_user, except: :show
+
   def new
-    @game = Game.new
   end
 
   def create
   end
   
   def show
+  end
+
+  private
+
+  def redirect_ileligible_user
+    redirect_to root_path unless user_signed_in? && current_user.id == 1
   end
 end
