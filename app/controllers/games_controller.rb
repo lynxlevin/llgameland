274d6b5176860b8c9791134d37dfd1a67b2754de
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 18:13:53 +0900

    ゲーム登録画面の作成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,7 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    if current_user.id == 1 && game.save
+    if current_user.id == 1 && @game.save
       redirect_to root_path
     else
       render :new
