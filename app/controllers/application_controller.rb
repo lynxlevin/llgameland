a36d8d9e792f324535fadf69e652bf15ea1e2bab
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 09:17:27 +0900

    edit userの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password, :username, :email]
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: update_attrs)
   end
 end
