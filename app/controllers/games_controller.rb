841818cbf1db2552da95ef3ccc34027c44f7082b
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-29 18:04:01 +0900

    アクションとストロングパラメーター

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,16 @@ class GamesController < ApplicationController
   before_action :redirect_ileligible_user, except: :show
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if current_user.id == 1 && game.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def show
@@ -14,5 +21,9 @@ class GamesController < ApplicationController
 
   def redirect_ileligible_user
     redirect_to root_path unless user_signed_in? && current_user.id == 1
+  end
+
+  def game_params
+    params.require(:game).permit(:image, :name, :description)
   end
 end
