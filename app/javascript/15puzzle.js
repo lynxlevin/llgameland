7cef2fa4993b5a81744b86ebd40c018fb72ed7c7
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-01 23:16:50 +0900

    Revert "Revert "Revert "マスの数を選べる設定"""
    This reverts commit 156d5a1fae1fc253f1885270c484c228bb67527c.

--- app/javascript/15puzzle.js
+++ app/javascript/15puzzle.js
@@ -2,79 +2,73 @@ window.addEventListener("load", () => {
     if (!checkPath()) {
       return null;
     }
-    const gameRestart = document.getElementById("restart-game");
-    gameRestart.textContent = "START";
-    gameRestart.addEventListener("click", () => {
-      let board = document.getElementById("board");
-      board.innerHTML = "";
-      let input = Number(document.getElementById("input").value);
-      "use strict";
-      let tiles = [];
-      for (let i = 0 ; i < input ; i++) {
-        let tr = document.createElement("tr");
-        for (let j = 0 ; j < input ; j++) {
-          let index = i * input + j
-          let td = document.createElement("td");
-          td.className = "tile";
-          td.index = index;
-          td.id = "tile" + (index + 1);
-          td.textContent = index == (input * input - 1) ? "" : (index + 1);
-          td.onclick = click;
-          tr.appendChild(td);
-          tiles.push(td)
-        }
-        board.appendChild(tr);
+    "use strict";
+    let tiles = [];
+    let board = document.getElementById("board");
+    for (let i = 0 ; i < 4 ; i++) {
+      let tr = document.createElement("tr");
+      for (let j = 0 ; j < 4 ; j++) {
+        let index = i * 4 + j
+        let td = document.createElement("td");
+        td.className = "tile";
+        td.index = index;
+        td.id = "tile" + (index + 1);
+        td.textContent = index == 15 ? "" : (index + 1);
+        td.onclick = click;
+        tr.appendChild(td);
+        tiles.push(td)
       }
-      for (let i = 0 ; i < 1000 ; i++) {
-        click({ srcElement: {index: Math.floor(Math.random() * (input * input))}})
-      }
-      document.getElementById("info").textContent = "";
-        function click(e) {
-          let clicked = e.srcElement.index;
-          let blank = document.getElementById(`tile${input * input}`).index;
-          let distance = blank - clicked;
-          if (Math.floor(clicked / input) == Math.floor(blank / input)) {// clickedとblankが同じ行なら
-            for (let k = 0 ; k < Math.abs(distance) ; k++) {
-              swap(blank, blank - Math.sign(distance));
-              blank = document.getElementById(`tile${input * input}`).index;
-            }
-            judge();
-          } else if (clicked % input == blank % input) {
-            for (let k = 0 ; k < Math.abs(distance) / input ; k++) {// clickedとblankが同じ列なら
-              swap(blank, blank - Math.sign(distance) * input);
-              blank = document.getElementById(`tile${input * input}`).index;
-            }
-            judge();
-          }
-        }
-        function swap(i, j) {
-          let tileI = tiles[i];
-          let tileJ = tiles[j];
-          let tmpTextContent = tileI.textContent;
-          let tmpId = tileI.id;
-          tileI.textContent = tileJ.textContent;
-          tileI.id = tileJ.id;
-          tileJ.textContent = tmpTextContent;
-          tileJ.id = tmpId;
-        }
-        function judge() {
-          let goal = 0
-          tiles.forEach( function(tile) {
-            if (tile.textContent == (tile.index + 1) ||
-                (tile.textContent == "" && tile.index == (input * input - 1))) {
-              goal = goal;
-            } else {
-              goal = goal + 1;
-            }
-          })
-          if (goal == 0) {
-            document.getElementById("info").textContent = " You've DONE IT!!";
-          }
-        }
-      })
-      function checkPath() {
-        const path = location.pathname;
-        if (path === "/games/15puzzle") {return true};
-      }
-    });
+      board.appendChild(tr);
+    }
+    for (let i = 0 ; i < 1000 ; i++) {
+      click({ srcElement: {index: Math.floor(Math.random() * 16)}})
+    }
+    document.getElementById("info").textContent = "";
     
+  function click(e) {
+    let clicked = e.srcElement.index;
+    let blank = document.getElementById("tile16").index;
+    let distance = blank - clicked;
+    if (Math.floor(clicked / 4) == Math.floor(blank / 4)) {// clickedとblankが同じ行なら
+      for (let k = 0 ; k < Math.abs(distance) ; k++) {
+        swap(blank, blank - Math.sign(distance));
+        blank = document.getElementById("tile16").index;
+      }
+      judge();
+    } else if (clicked % 4 == blank % 4) {
+      for (let k = 0 ; k < Math.abs(distance) / 4 ; k++) {// clickedとblankが同じ列なら
+        swap(blank, blank - Math.sign(distance) * 4);
+        blank = document.getElementById("tile16").index;
+      }
+      judge();
+    }
+  }
+  function swap(i, j) {
+    let tileI = tiles[i];
+    let tileJ = tiles[j];
+    let tmpTextContent = tileI.textContent;
+    let tmpId = tileI.id;
+    tileI.textContent = tileJ.textContent;
+    tileI.id = tileJ.id;
+    tileJ.textContent = tmpTextContent;
+    tileJ.id = tmpId;
+  }
+  function judge() {
+    let goal = 0
+    tiles.forEach( function(tile) {
+      if (tile.textContent == (tile.index + 1) ||
+          (tile.textContent == "" && tile.index == 15)) {
+        goal = goal;
+      } else {
+        goal = goal + 1;
+      }
+    })
+    if (goal == 0) {
+      document.getElementById("info").textContent = " You've DONE IT!!";
+    }
+  }
+})
+function checkPath() {
+  const path = location.pathname;
+  if (path === "/games/15puzzle") {return true};
+}
