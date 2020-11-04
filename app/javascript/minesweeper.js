676806bdfbf4881a44a778c144368020be3d07db
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-04 12:30:41 +0900

    input1のid変更

--- app/javascript/minesweeper.js
+++ app/javascript/minesweeper.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
   }
   const gameRestart = document.getElementById("restart-game");
   gameRestart.textContent = "START";
-  document.getElementById("input1-message").textContent = "※マス目の数を指定（○×○マス）"
+  document.getElementById("select1-message").textContent = "※マス目の数を指定（○×○マス）"
   document.getElementById("input2-message").textContent = "※どんぐりの数を指定"
   let plowBtn = document.getElementById("game-btn1");
   let acornBtn = document.getElementById("game-btn2");
@@ -12,11 +12,11 @@ window.addEventListener("load", () => {
   document.getElementById("game-btn-text1").textContent = "耕す";
   acornBtn.className = "acorn-btn game-btn-off";
   document.getElementById("game-btn-text2").textContent = "どんぐりマークをつける";
-  let input1 = document.getElementById("input1");
+  let select1 = document.getElementById("select1");
   let input2 = document.getElementById("input2");
-  input1.value = 10;
+  select1.value = 10;
   input2.value = 15;
-  setInputButton();
+  // setInputButton();
   
   gameRestart.addEventListener("click", () => {
     "use strict";
@@ -27,24 +27,24 @@ window.addEventListener("load", () => {
     let acornModeCode = false;
     let gameInAction = true;
     let board = document.getElementById("board");
-    let input1Value = Number(document.getElementById("input1").value);
+    let select1Value = Number(document.getElementById("select1").value);
     gameRestart.textContent = "RESTART";
     board.innerHTML = "";
     board.className = "minesweeper-board";
     let tiles = [];
-    for (let i = 0 ; i < input1Value ; i++) {
+    for (let i = 0 ; i < select1Value ; i++) {
       let tr = document.createElement("tr");
-      for (let j = 0 ; j < input1Value ; j++) {
-        let index = i * input1Value + j
+      for (let j = 0 ; j < select1Value ; j++) {
+        let index = i * select1Value + j
         let td = document.createElement("td");
         td.className = "tile-closed";
         td.index = index;
         // td.id = `tile${index}`
-        td.style.height = `${63 / input1Value}vmin`;
-        td.style.width = `${63 / input1Value}vmin`;
-        td.style.fontSize = `${240 / input1}px`;
+        td.style.height = `${63 / select1Value}vmin`;
+        td.style.width = `${63 / select1Value}vmin`;
+        td.style.fontSize = `${240 / select1}px`;
         td.onclick = click;
-        td.oncontextmenu = rightClick;
+        // td.oncontextmenu = rightClick;
         tr.appendChild(td);
         tiles.push(td)
       }
@@ -58,9 +58,9 @@ window.addEventListener("load", () => {
     
     function buryAcorn(acorns) {
       let tileIndexes = [];
-      let input1Value = Number(document.getElementById("input1").value);
+      let select1Value = Number(document.getElementById("select1").value);
       let input2Value = Number(document.getElementById("input2").value);
-      for (let i = 0 ; i < input1Value * input1Value ; i++) {
+      for (let i = 0 ; i < select1Value * select1Value ; i++) {
         tileIndexes.push(i);
       }
       for (let i = 0 ; i < input2Value ; i++) {
@@ -71,31 +71,31 @@ window.addEventListener("load", () => {
       }
     }
     function countNearbyAcorns(tiles, acorns) {
-      let input1Value = Number(document.getElementById("input1").value);
+      let select1Value = Number(document.getElementById("select1").value);
       tiles.forEach((tile, i) => {
         let acornCount = 0;
-        if (Math.floor(i / input1Value) != 0 && acorns.includes(i - input1Value)) {// 一番上でない → 上を見る
+        if (Math.floor(i / select1Value) != 0 && acorns.includes(i - select1Value)) {// 一番上でない → 上を見る
           acornCount++;
         }
-        if (Math.floor(i / input1Value) != (input1Value - 1) && acorns.includes(i + input1Value)) {// 一番下でない → 下を見る
+        if (Math.floor(i / select1Value) != (select1Value - 1) && acorns.includes(i + select1Value)) {// 一番下でない → 下を見る
           acornCount++;
         }
-        if (Math.floor(i % input1Value) != 0 && acorns.includes(i - 1)) {// 一番左でない → 左を見る
+        if (Math.floor(i % select1Value) != 0 && acorns.includes(i - 1)) {// 一番左でない → 左を見る
           acornCount++;
         }
-        if (Math.floor(i % input1Value) != (input1Value - 1) && acorns.includes(i + 1)) {// 一番右でない → 右を見る
+        if (Math.floor(i % select1Value) != (select1Value - 1) && acorns.includes(i + 1)) {// 一番右でない → 右を見る
           acornCount++;
         }
-        if (Math.floor(i / input1Value) != 0 && Math.floor(i % input1Value) != 0 && acorns.includes(i - input1Value - 1)) {// 一番上でなく、左でもない → 左上を見る
+        if (Math.floor(i / select1Value) != 0 && Math.floor(i % select1Value) != 0 && acorns.includes(i - select1Value - 1)) {// 一番上でなく、左でもない → 左上を見る
           acornCount++;
         }
-        if (Math.floor(i / input1Value) != 0 && Math.floor(i % input1Value) != (input1Value - 1) && acorns.includes(i - input1Value + 1)) {// 一番上でなく、右でもない → 右上を見る
+        if (Math.floor(i / select1Value) != 0 && Math.floor(i % select1Value) != (select1Value - 1) && acorns.includes(i - select1Value + 1)) {// 一番上でなく、右でもない → 右上を見る
           acornCount++;
         }
-        if (Math.floor(i / input1Value) != (input1Value - 1) && Math.floor(i % input1Value) != 0 && acorns.includes(i + input1Value - 1)) {// 一番下でなく、左でもない → 左下を見る
+        if (Math.floor(i / select1Value) != (select1Value - 1) && Math.floor(i % select1Value) != 0 && acorns.includes(i + select1Value - 1)) {// 一番下でなく、左でもない → 左下を見る
           acornCount++;
         }
-        if (Math.floor(i / input1Value) != (input1Value - 1) && Math.floor(i % input1Value) != (input1Value - 1) && acorns.includes(i + input1Value + 1)) {// 一番下でなく、右でもない → 右下を見る
+        if (Math.floor(i / select1Value) != (select1Value - 1) && Math.floor(i % select1Value) != (select1Value - 1) && acorns.includes(i + select1Value + 1)) {// 一番下でなく、右でもない → 右下を見る
           acornCount++;
         }
         if (acornCount != 0) {
@@ -135,29 +135,29 @@ window.addEventListener("load", () => {
     }
     function clickBlank(clicked) {
       let i = clicked.index;
-      if (Math.floor(i / input1Value) != 0 && tiles[i - input1Value].className != "tile-open") {// 一番上でない → 上をクリック
-        tiles[i - input1Value].click();
+      if (Math.floor(i / select1Value) != 0 && tiles[i - select1Value].className != "tile-open") {// 一番上でない → 上をクリック
+        tiles[i - select1Value].click();
       }
-      if (Math.floor(i / input1Value) != (input1Value - 1) && tiles[i + input1Value].className != "tile-open") {// 一番下でない → 下をクリック
-        tiles[i + input1Value].click();
+      if (Math.floor(i / select1Value) != (select1Value - 1) && tiles[i + select1Value].className != "tile-open") {// 一番下でない → 下をクリック
+        tiles[i + select1Value].click();
       }
-      if (Math.floor(i % input1Value) != 0 && tiles[i - 1].className != "tile-open") {// 一番左でない → 左をクリック
+      if (Math.floor(i % select1Value) != 0 && tiles[i - 1].className != "tile-open") {// 一番左でない → 左をクリック
         tiles[i - 1].click();
       }
-      if (Math.floor(i % input1Value) != (input1Value - 1) && tiles[i + 1].className != "tile-open") {// 一番右でない → 右をクリック
+      if (Math.floor(i % select1Value) != (select1Value - 1) && tiles[i + 1].className != "tile-open") {// 一番右でない → 右をクリック
         tiles[i + 1].click();
       }
-      if (Math.floor(i / input1Value) != 0 && Math.floor(i % input1Value) != 0 && tiles[i - input1Value - 1].className != "tile-open") {// 一番上でなく、左でもない → 左上をクリック
-        tiles[i - input1Value - 1].click();
+      if (Math.floor(i / select1Value) != 0 && Math.floor(i % select1Value) != 0 && tiles[i - select1Value - 1].className != "tile-open") {// 一番上でなく、左でもない → 左上をクリック
+        tiles[i - select1Value - 1].click();
       }
-      if (Math.floor(i / input1Value) != 0 && Math.floor(i % input1Value) != (input1Value - 1) && tiles[i - input1Value + 1].className != "tile-open") {// 一番上でなく、右でもない → 右上をクリック
-        tiles[i - input1Value + 1].click();
+      if (Math.floor(i / select1Value) != 0 && Math.floor(i % select1Value) != (select1Value - 1) && tiles[i - select1Value + 1].className != "tile-open") {// 一番上でなく、右でもない → 右上をクリック
+        tiles[i - select1Value + 1].click();
       }
-      if (Math.floor(i / input1Value) != (input1Value - 1) && Math.floor(i % input1Value) != 0 && tiles[i + input1Value - 1].className != "tile-open") {// 一番下でなく、左でもない → 左下をクリック
-        tiles[i + input1Value - 1].click();
+      if (Math.floor(i / select1Value) != (select1Value - 1) && Math.floor(i % select1Value) != 0 && tiles[i + select1Value - 1].className != "tile-open") {// 一番下でなく、左でもない → 左下をクリック
+        tiles[i + select1Value - 1].click();
       }
-      if (Math.floor(i / input1Value) != (input1Value - 1) && Math.floor(i % input1Value) != (input1Value - 1) && tiles[i + input1Value + 1].className != "tile-open") {// 一番下でなく、右でもない → 右下をクリック
-        tiles[i + input1Value + 1].click();
+      if (Math.floor(i / select1Value) != (select1Value - 1) && Math.floor(i % select1Value) != (select1Value - 1) && tiles[i + select1Value + 1].className != "tile-open") {// 一番下でなく、右でもない → 右下をクリック
+        tiles[i + select1Value + 1].click();
       }
     }
     function rightClick(e) {
@@ -194,20 +194,20 @@ window.addEventListener("load", () => {
       gameInAction = false;
     }
   })
-  function setInputButton() {
-    document.getElementById("input1-up").addEventListener("click", () => {
-      input1.value++;
-    });
-    document.getElementById("input1-down").addEventListener("click", () => {
-      input1.value--;
-    });
-    document.getElementById("input2-up").addEventListener("click", () => {
-      input2.value++;
-    });
-    document.getElementById("input2-down").addEventListener("click", () => {
-      input2.value--;
-    });
-  }
+  // function setInputButton() {
+  //   document.getElementById("select1-up").addEventListener("click", () => {
+  //     select1.value++;
+  //   });
+  //   document.getElementById("select1-down").addEventListener("click", () => {
+  //     select1.value--;
+  //   });
+  //   document.getElementById("input2-up").addEventListener("click", () => {
+  //     input2.value++;
+  //   });
+  //   document.getElementById("input2-down").addEventListener("click", () => {
+  //     input2.value--;
+  //   });
+  // }
   function checkPath() {
       const path = location.pathname;
       if (path === "/games/minesweeper") {return true};
