fbc3f354c48e954f3b0319bd9a9960e9817ad499
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-11 12:34:06 +0900

    README作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create(display_name: 'まるばつゲーム', game_name: 'tictactoe', description: '2人で遊べるまるばつゲームです。', image_url: 'http://localhost:3000/assets/1tictactoe-da852c751b2663aff87765085de3fd320a81395e13f044965e01a1218c99a556.png',)
+Game.create(display_name: '15 Puzzle', game_name: '15puzzle', description: 'バラバラになった数字を、図のように順番に並べ替えてください。', image_url: 'http://localhost:3000/assets/215puzzle-3736d7dc552ff4897ccf29f392826e9e3468b8b0b01835f1ecc3ae8de8f0c7a1.png',)
+Game.create(display_name: 'どんぐり探し', game_name: 'minesweeper', description: 'マインスイーパ風ゲーム
+あなたの畑にリスがどんぐりを埋めてしまったようです。
+優しいあなたは、どんぐりのあるところは避けて、畑を耕してください。', image_url: 'http://localhost:3000/assets/3minesweeper-84d270ae146dc590f48a399dfb267ccdebe212361c28d1ab13aad5f9ebd9fd5d.png',)
