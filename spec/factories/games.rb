757db6cdec8cc369b3f42bb01e3552ac442b2b7f
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-13 16:10:06 +0900

    gameモデルの単体テストコード

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :game do
-    
+    display_name { Faker::Game.title }
+    game_name    { display_name }
+    description  { Faker::Lorem.paragraphs }
+    image_url    { Faker::LoremPixel.image(size: "50x60") }
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @game = FactoryBot.build(:game)
+  end
+
+  describe 'ゲーム新規登録' do
+    context '正常な動作の確認' do
+      it 'display_nameとgame_nameが存在すれば登録できること' do
+        expect(@game).to be_valid
+      end
+
+      it 'display_nameとgame_nameが存在すれば他の項目が空欄でも登録できること' do
+        @game.description = ""
+        @game.image_url = ""
+        expect(@game).to be_valid
+      end
+    end
+
+    context '登録できない時の確認' do
+      it 'display_nameが空欄では登録ができないこと' do
+        @game.display_name = nil
+        @game.valid?
+        expect(@game.errors.full_messages).to include("Display name can't be blank")
+      end
+
+      it 'display_nameが既に使用されているときは登録ができないこと' do
+        @game.save
+        another_game = FactoryBot.build(:game)
+        another_game.display_name = @game.display_name
+        another_game.valid?
+        expect(another_game.errors.full_messages).to include('Display name has already been taken')
+      end
+
+      it 'game_nameが空欄では登録ができないこと' do
+        @game.game_name = nil
+        @game.valid?
+        expect(@game.errors.full_messages).to include("Game name can't be blank")
+      end
+
+      it 'game_nameが既に使用されているときは登録ができないこと' do
+        @game.save
+        another_game = FactoryBot.build(:game)
+        another_game.game_name = @game.game_name
+        another_game.valid?
+        expect(another_game.errors.full_messages).to include('Game name has already been taken')
+      end
+    end
+  end
 end
