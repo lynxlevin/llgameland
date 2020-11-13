3485d4926c3786df863582e241f484a1253269a2
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-13 17:06:01 +0900

    gamesマイグレーションにuniqueを追加

--- db/migrate/20201029081447_create_games.rb
+++ db/migrate/20201029081447_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-      t.string :display_name, null: false
-      t.string :game_name,    null: false
+      t.string :display_name, null: false, unique: true
+      t.string :game_name,    null: false, unique: true
       t.text   :description
       t.string :image_url
       t.timestamps
