cd818b685d0503d659f285f3c14856a4ecee1683
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-13 16:21:09 +0900

    case_sensitiveの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 4..128
 
