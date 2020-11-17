57570d445affa8625707409d38317a563945f7ce
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-17 11:22:58 +0900

    15puzzle タイマーの導入

--- app/javascript/15puzzle.js
+++ app/javascript/15puzzle.js
@@ -1,100 +1,123 @@
 window.addEventListener("load", () => {
-    if (!checkPath()) {
-      return null;
-    }
-    revealContents();
-    hideContents();
-    const gameRestart = document.getElementById("restart-game");
-    gameRestart.textContent = "START";
-    document.getElementById("select1-message").textContent = "※マス目の数を指定（○×○マス）"
-    document.getElementById("select1").value = 4
+  if (!checkPath()) {
+    return null;
+  }
+  revealContents();
+  hideContents();
+  const gameRestart = document.getElementById("restart-game");
+  gameRestart.textContent = "START";
+  document.getElementById("select1-message").textContent = "※マス目の数を指定（○×○マス）"
+  document.getElementById("select1").value = 4
 
-    document.getElementById("inputs-wrapper").style.height = "65px";
-    gameRestart.addEventListener("click", () => {
-      gameRestart.textContent = "RESTART";
-      let board = document.getElementById("board");
-      board.innerHTML = "";
-      let select1 = Number(document.getElementById("select1").value);
-      "use strict";
-      let tiles = [];
-      for (let i = 0 ; i < select1 ; i++) {
-        let tr = document.createElement("tr");
-        for (let j = 0 ; j < select1 ; j++) {
-          let index = i * select1 + j
-          let td = document.createElement("td");
-          td.className = "tile";
-          td.index = index;
-          td.style.height = `${75 / select1}vmin`;
-          td.style.width = `${75 / select1}vmin`;
-          td.style.fontSize = `${120 / select1}px`;
-          td.id = "tile" + (index + 1);
-          td.textContent = index == (select1 * select1 - 1) ? "" : (index + 1);
-          td.onclick = click;
-          tr.appendChild(td);
-          tiles.push(td)
-        }
-        board.appendChild(tr);
+  document.getElementById("inputs-wrapper").style.height = "65px";
+  let gameTimer = NaN;
+  gameRestart.addEventListener("click", () => {
+    gameRestart.textContent = "RESTART";
+    clearInterval(gameTimer);
+    document.getElementById("timer").textContent = `00:00:00`;
+    let firstClick = false;
+    let board = document.getElementById("board");
+    board.innerHTML = "";
+    let select1 = Number(document.getElementById("select1").value);
+    "use strict";
+    let tiles = [];
+    for (let i = 0 ; i < select1 ; i++) {
+      let tr = document.createElement("tr");
+      for (let j = 0 ; j < select1 ; j++) {
+        let index = i * select1 + j
+        let td = document.createElement("td");
+        td.className = "tile";
+        td.index = index;
+        td.style.height = `${75 / select1}vmin`;
+        td.style.width = `${75 / select1}vmin`;
+        td.style.fontSize = `${120 / select1}px`;
+        td.id = "tile" + (index + 1);
+        td.textContent = index == (select1 * select1 - 1) ? "" : (index + 1);
+        td.onclick = click;
+        tr.appendChild(td);
+        tiles.push(td)
       }
-      for (let i = 0 ; i < 1000 + (select1 * select1 * select1) ; i++) {
-        click({ srcElement: {index: Math.floor(Math.random() * (select1 * select1))}})
+      board.appendChild(tr);
+    }
+    for (let i = 0 ; i < 1000 + (select1 * select1 * select1) ; i++) {
+      click({ srcElement: {index: Math.floor(Math.random() * (select1 * select1))}})
+    }
+    firstClick = true;
+    document.getElementById("info1").textContent = "";
+    function click(e) {
+      if (firstClick) {
+        startTimer();
+        firstClick = false;
       }
-      document.getElementById("info1").textContent = "";
-        function click(e) {
-          let clicked = e.srcElement.index;
-          let blank = document.getElementById(`tile${select1 * select1}`).index;
-          let distance = blank - clicked;
-          if (Math.floor(clicked / select1) == Math.floor(blank / select1)) {// clickedとblankが同じ行なら
-            for (let k = 0 ; k < Math.abs(distance) ; k++) {
-              swap(blank, blank - Math.sign(distance));
-              blank = document.getElementById(`tile${select1 * select1}`).index;
-            }
-            judge();
-          } else if (clicked % select1 == blank % select1) {
-            for (let k = 0 ; k < Math.abs(distance) / select1 ; k++) {// clickedとblankが同じ列なら
-              swap(blank, blank - Math.sign(distance) * select1);
-              blank = document.getElementById(`tile${select1 * select1}`).index;
-            }
-            judge();
-          }
+      let clicked = e.srcElement.index;
+      let blank = document.getElementById(`tile${select1 * select1}`).index;
+      let distance = blank - clicked;
+      if (Math.floor(clicked / select1) == Math.floor(blank / select1)) {// clickedとblankが同じ行なら
+        for (let k = 0 ; k < Math.abs(distance) ; k++) {
+          swap(blank, blank - Math.sign(distance));
+          blank = document.getElementById(`tile${select1 * select1}`).index;
         }
-        function swap(i, j) {
-          let tileI = tiles[i];
-          let tileJ = tiles[j];
-          let tmpTextContent = tileI.textContent;
-          let tmpId = tileI.id;
-          tileI.textContent = tileJ.textContent;
-          tileI.id = tileJ.id;
-          tileJ.textContent = tmpTextContent;
-          tileJ.id = tmpId;
+        judge();
+      } else if (clicked % select1 == blank % select1) {
+        for (let k = 0 ; k < Math.abs(distance) / select1 ; k++) {// clickedとblankが同じ列なら
+          swap(blank, blank - Math.sign(distance) * select1);
+          blank = document.getElementById(`tile${select1 * select1}`).index;
         }
-        function judge() {
-          let goal = 0
-          tiles.forEach( function(tile) {
-            if (tile.textContent == (tile.index + 1) ||
-                (tile.textContent == "" && tile.index == (select1 * select1 - 1))) {
-              goal = goal;
-            } else {
-              goal = goal + 1;
-            }
-          })
-          if (goal == 0) {
-            document.getElementById("info1").textContent = " You've DONE IT!!";
-          }
+        judge();
+      }
+    }
+    function swap(i, j) {
+      let tileI = tiles[i];
+      let tileJ = tiles[j];
+      let tmpTextContent = tileI.textContent;
+      let tmpId = tileI.id;
+      tileI.textContent = tileJ.textContent;
+      tileI.id = tileJ.id;
+      tileJ.textContent = tmpTextContent;
+      tileJ.id = tmpId;
+    }
+    function judge() {
+      let goal = 0
+      tiles.forEach( function(tile) {
+        if (tile.textContent == (tile.index + 1) ||
+            (tile.textContent == "" && tile.index == (select1 * select1 - 1))) {
+          goal = goal;
+        } else {
+          goal = goal + 1;
         }
       })
-      function checkPath() {
-        const path = location.pathname;
-        if (path === "/games/15puzzle") {return true};
+      if (goal == 0) {
+        clearInterval(gameTimer);
+        document.getElementById("info1").textContent = " You've DONE IT!!";
       }
-      function revealContents() {
-        document.getElementById("top-btn-wrapper").style.display = "";
-        document.getElementById("inputs-wrapper").style.display = "";
-        document.getElementById("select-boxes-wrapper").style.display = "";
-        document.getElementById("game-info-wrapper").style.display = "";
-      }
-      function hideContents() {
-        document.getElementById("select2-div").style.display = "none";
-        document.getElementById("input-info").style.display = "none";
-      }
-    });
+    }
+  })
+  function startTimer() {
+    let elapsedTime = 0;
+    gameTimer = setInterval(() => {
+      elapsedTime ++;
+      let hour = Math.floor(elapsedTime / 3600);
+      let minute = Math.floor(elapsedTime / 60);
+      let second = Math.floor(elapsedTime % 60);
+      hour = ("0" + hour).slice(-2);
+      minute = ("0" + minute).slice(-2);
+      second = ("0" + second).slice(-2);
+      document.getElementById("timer").textContent = `${hour}:${minute}:${second}`;
+    }, 1000);
+  }
+  function checkPath() {
+    const path = location.pathname;
+    if (path === "/games/15puzzle") {return true};
+  }
+  function revealContents() {
+    document.getElementById("top-btn-wrapper").style.display = "";
+    document.getElementById("inputs-wrapper").style.display = "";
+    document.getElementById("select-boxes-wrapper").style.display = "";
+    document.getElementById("game-info-wrapper").style.display = "";
+  }
+  function hideContents() {
+    document.getElementById("select2-div").style.display = "none";
+    document.getElementById("input-info").style.display = "none";
+  }
+});
     
