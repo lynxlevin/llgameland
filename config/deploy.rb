625a264db92053fd7795fe71691d401160cf1d84
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-17 10:14:43 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.6.5' # ruby -v で確認
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/Chihirodaisuki0617.pem'] 
+                                  keys: ['~/.ssh/awsec2.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
