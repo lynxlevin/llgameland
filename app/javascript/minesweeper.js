9bf7ab105b0a7dd9c999179c7aa6c1b877c5bb49
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-19 11:13:39 +0900

    Revert "Update minesweeper.js"
    This reverts commit cb966bd9aac6b5968970c6fa6ca3ab3da7deb306.

--- app/javascript/minesweeper.js
+++ app/javascript/minesweeper.js
@@ -66,7 +66,7 @@ window.addEventListener("load", () => {
           td.style.width = `${65 / select1Value}vmin`;
           td.style.fontSize = `${35 / select1Value}vmin`;
           td.addEventListener("mousedown", mouseDown);
-          td.addEventListener("touchstart", touchStart);
+          td.addEventListener("touchstart", mouseDown);
           tr.appendChild(td);
           tiles.push(td)
         }
