9818dc497249acc9bda47aaf266d6653bb648912
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-19 11:14:06 +0900

    Revert "タッチ用の修正"
    This reverts commit 9c6fb3bb9bdebbd018c88589e49ad5a88aa8b8a4.

--- app/javascript/minesweeper.js
+++ app/javascript/minesweeper.js
@@ -144,8 +144,7 @@ window.addEventListener("load", () => {
       }
     }
     function mouseDown(e) {
-      // e.preventDefault();
-      tiles.forEach( tile => { tile.removeEventListener("touchstart", mouseDown) });
+      e.preventDefault();
       e.srcElement.addEventListener("mouseup", mouseUp);
       e.srcElement.addEventListener("mouseleave", mouseLeave);
       e.srcElement.addEventListener("touchend", mouseUp);
@@ -162,7 +161,6 @@ window.addEventListener("load", () => {
       }
     }
     function mouseUp(e) {
-      tiles.forEach( tile => { tile.addEventListener("touchstart", mouseDown) });
       e.srcElement.removeEventListener("touchend", mouseUp);
       e.srcElement.removeEventListener("touchmove", mouseLeave);
       e.srcElement.removeEventListener("mouseup", mouseUp);
@@ -176,7 +174,6 @@ window.addEventListener("load", () => {
       }
     }
     function mouseLeave(e) {
-      tiles.forEach( tile => { tile.addEventListener("touchstart", mouseDown) });
       e.srcElement.removeEventListener("touchend", mouseUp);
       e.srcElement.removeEventListener("touchmove", mouseLeave);
       e.srcElement.removeEventListener("mouseup", mouseUp);
