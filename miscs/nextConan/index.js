ddb95a9efe716fbc444fceb3af8618487891e7da
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-08-22 10:25:33 +0900

    slackをファンクション化 & reをif文で

--- miscs/nextConan/index.js
+++ miscs/nextConan/index.js
@@ -7,12 +7,13 @@ const { JSDOM } = require("jsdom");
   const url1 = `https://www.ytv.co.jp/conan/archive/k${getPrevSaturday()}.html`;
   const response1 = await axios.get(url1);
   const dom1 = new JSDOM(response1.data);
-  const re1 = new RegExp(/「(.*)」/);
-  const prevTitleForShowing = dom1.window.document.getElementsByClassName('oa_title')[0].textContent;
-  const prevTitle = prevTitleForShowing.match(re1)[1];
+  const re10 = new RegExp(/「(.*)」/); // タイトル部分を取得する
   const re11 = new RegExp(/(.*)（/); // （前編）などを取り除く
   const re12 = new RegExp(/(.*)\(/); // (パーフェクトゲーム)などを取り除く
-  const prevTitleForMatching = zenkaku2Hankaku(prevTitle.match(re11)[1].match(re12)[1]);
+  const prevTitleForShowing = dom1.window.document.getElementsByClassName('oa_title')[0].textContent;
+  let prevTitleForMatching = zenkaku2Hankaku(prevTitleForShowing.match(re10)[1]);
+  if (prevTitleForMatching.match(re11)) prevTitleForMatching = prevTitleForMatching.match(re11)[1];
+  if (prevTitleForMatching.match(re12)) prevTitleForMatching = prevTitleForMatching.match(re12)[1];
 
   // まとめサイトから、タイトルと重要度が記載された部分を取得
   const url2 = 'https://www.miyachiman.com/entry/conan-black';
@@ -29,6 +30,10 @@ const { JSDOM } = require("jsdom");
   const re2 = new RegExp(/【(.*)】/);
   const importance = titleMatch.length === 0 ? "重要回ではありません" : '重要度' + titleMatch[0].textContent.match(re2)[0];
   const message = `前回のコナンは「${prevTitleForShowing}」${importance}`;
+  sendSlack(message);
+})();
+
+function sendSlack(message) {
   const slackUrl = 'https://slack.com/api/chat.postMessage';
   const slackMessage = {
     "channel": process.env.CONANSLACKCHANNEL,
@@ -36,9 +41,11 @@ const { JSDOM } = require("jsdom");
   }
   const slackHeader = { headers: { authorization: `Bearer ${process.env.CONANSLACKTOKEN}` } };
   axios.post(slackUrl, slackMessage, slackHeader);
-})();
+}
 
 function getPrevSaturday() {
+  // return '20210814'
+  return '20210501'
   const prevSaturday = new Date(); // この時点ではまだtoday
   prevSaturday.setDate(prevSaturday.getDate() - prevSaturday.getDay() - 1)
   const year = ("0000" + prevSaturday.getFullYear().toString()).slice(-4);
